fce8f6ef66a9aa2b24904431f2538d146f5a54b7
Robotophile <ksyunya.iv@gmail.com>
2025-02-10 13:06:32 +0300

    make function for dublicate

--- ivanova.ksenia/K2/main.cpp
+++ ivanova.ksenia/K2/main.cpp
@@ -1,4 +1,34 @@
-int main()
+#include <iostream>
+
+struct FwdList {
+  int value;
+  FwdList* next;
+};
+
+FwdList* duplicate(FwdList* head, size_t index, size_t count)
 {
-  
+  FwdList* current = head;
+
+  for (size_t i = 0; i < index; i++)
+  {
+    current = current->next;
+  }
+
+  if (!current)
+  {
+    std::cerr << "Out of range" << '\n';
+    return nullptr;
+  }
+
+  for (size_t i = 0; i < count; ++i) {
+    FwdList* newNode = new FwdList{current->value, current->next};
+    current->next = newNode;
+    current = newNode;
+  }
+  return current;
+}
+
+int main() 
+{
+
 }
