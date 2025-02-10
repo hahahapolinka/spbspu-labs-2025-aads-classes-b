7319d1cff993cb789011173b8eebaa4ab3e7af00
Savishna <bravissimo28@mail.ru>
2025-02-10 12:38:13 +0300

    added main

--- averenkov.eugene/K2/main.cpp
+++ averenkov.eugene/K2/main.cpp
@@ -17,8 +17,16 @@ FwdList* ListManip(FwdList* head, size_t number, size_t count)
   FwdList* dubl = correct->next;
   for (size_t i = 0; i < count; i++)
   {
-    correct->next = correct;
-    correct = correct->next;
+    try
+    {
+      FwdList* newList = new FwdList{correct->value, nullptr};
+      correct->next = newList;
+      correct = newList;
+    }
+    catch (...)
+    {
+      throw;
+    }
   }
   correct->next = dubl;
   return point;
@@ -26,4 +34,36 @@ FwdList* ListManip(FwdList* head, size_t number, size_t count)
 
 int main()
 {
-  constexpr size_t size = 10;
+  constexpr int size = 10;
+  FwdList* head = new FwdList{0, nullptr};
+  FwdList* tail = head;
+  for (int i = 1; i < size; ++i)
+  {
+    tail->next = new FwdList{i, nullptr};
+    tail = tail->next;
+  }
+  size_t pos, count;
+  while (std::cin >> pos >> count)
+  {
+    try
+    {
+      ListManip(head, pos, count);
+    }
+    catch (...)
+    {
+      std::cerr << "Error";
+    }
+  }
+  FwdList* current = head;
+  while (current)
+  {
+    std::cout << current->value;
+    if (current->next) std::cout << " ";
+    FwdList* temp = current;
+    current = current->next;
+    delete temp;
+  }
+  std::cout << "\n";
+  return 0;
+}
+
