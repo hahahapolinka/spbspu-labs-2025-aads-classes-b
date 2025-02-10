64a987f88b5fc9ff414a1fa1a432e285d1b52346
Savishna <bravissimo28@mail.ru>
2025-02-10 11:54:41 +0300

    added func

--- averenkov.eugene/K2/main.cpp
+++ averenkov.eugene/K2/main.cpp
@@ -1,2 +1,29 @@
+#include <iostream>
+
+struct FwdList
+{
+  int value;
+  FwdList* next;
+};
+
+FwdList* ListManip(FwdList* head, size_t number, size_t count)
+{
+  FwdList* correct = head;
+  for (size_t i = 0; i < number; i++)
+  {
+    correct = correct->next;
+  }
+  FwdList* point = correct;
+  FwdList* dubl = correct->next;
+  for (size_t i = 0; i < count; i++)
+  {
+    correct->next = correct;
+    correct = correct->next;
+  }
+  correct->next = dubl;
+  return point;
+}
+
 int main()
-{}
+{
+  constexpr size_t size = 10;
