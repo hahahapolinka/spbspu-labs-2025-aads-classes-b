c6084e91894295e6edbe13dfdfff316ce48a4223
brev1ch <flirktv@gmail.com>
2025-02-10 11:59:05 +0300

    zagotovka

--- brevnov.nikita/K2/main.cpp
+++ brevnov.nikita/K2/main.cpp
@@ -1,1 +1,19 @@
-int main() {}
+#include <iostream>
+
+struct FwdList {
+  int value;
+  FwdList * next;
+};
+
+int main() 
+{
+  FwdList * head = new FwdList{0, nullptr};
+  FwdList * tail = head;
+  for (size_t i = 1; i < 10; i++)
+  {
+    tail->next = new FwdList{i, nullptr};
+    tail = tail->next;
+  }
+  int a = 0, b = 0;
+  while ( )
+}
