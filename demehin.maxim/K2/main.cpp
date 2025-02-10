e97d793594e9a1b2b7f10fdf439e7fb208a2b711
TvoyBatyA12343 <maxdem06@gmail.com>
2025-02-10 12:08:21 +0300

    implementation of creating list + input

--- demehin.maxim/K2/main.cpp
+++ demehin.maxim/K2/main.cpp
@@ -1,2 +1,46 @@
-int main()
+#include <iostream>
+
+struct FwdList
+{
+  int value;
+  FwdList * next;
+};
+
+FwdList* createDubls(FwdList* list, size_t num, size_t dubls)
 {}
+
+void freeList(FwdList* head)
+{}
+
+int main()
+{
+  FwdList* head = nullptr;
+  for (size_t i = 1; i < 11; i++)
+  {
+    try
+    {
+      FwdList* newNode = new FwdList{i, nullptr};
+      if (head == nullptr)
+      {
+        head = newNode;
+      }
+      else
+      {
+        newNode->next = head;
+        head = newNode;
+      }
+    }
+    catch (const std::bad_alloc&)
+    {
+      freeList(head);
+    }
+  }
+
+  size_t num = 0, dubls = 0;
+
+  while (std::cin << num << dubls)
+  {
+    createDubls(head, num, dubls);
+  }
+
+}
