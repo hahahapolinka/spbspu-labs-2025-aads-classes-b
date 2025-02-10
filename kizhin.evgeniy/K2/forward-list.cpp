bfe60499dcceb07b7740f7a21ec8bb43627d025c
Might be a Fox <e.kizhin@yandex.ru>
2025-02-10 12:03:25 +0300

    Introduce insertDuplicates function

--- kizhin.evgeniy/K2/forward-list.cpp
+++ kizhin.evgeniy/K2/forward-list.cpp
@@ -1,5 +1,6 @@
 #include "forward-list.hpp"
 #include <cassert>
+#include <stdexcept>
 
 namespace kizhin {
   void clear(FwdList*) noexcept;
@@ -19,6 +20,35 @@ kizhin::FwdList* kizhin::initList(int start, const int stop)
     throw;
   }
   return head;
+}
+
+kizhin::FwdList* kizhin::insertDuplicates(FwdList* const head, const std::size_t position,
+    const std::size_t size)
+{
+  assert(size != 0);
+  FwdList* target = head;
+  for (std::size_t i = 0; target != nullptr && i != position; ++i) {
+    target = target->next;
+  }
+  if (target == nullptr) {
+    throw std::logic_error("");
+  }
+  FwdList* first = new FwdList{ target->value, nullptr };
+  FwdList* last = first;
+  try {
+    for (std::size_t i = 1; i != size; ++i) {
+      FwdList* newNode = new FwdList{ target->value, nullptr };
+      last->next = newNode;
+      last = newNode;
+    }
+  } catch (...) {
+    clear(first);
+    throw;
+  }
+  FwdList* originalNext = target->next;
+  target->next = first;
+  last->next = originalNext;
+  return target;
 }
 
 void kizhin::clear(FwdList* head) noexcept
--- kizhin.evgeniy/K2/forward-list.hpp
+++ kizhin.evgeniy/K2/forward-list.hpp
@@ -1,6 +1,8 @@
 #ifndef SPBSPU_LABS_2025_AADS_CLASSES_B_KIZHIN_EVGENIY_K2_FORWARD_LIST_HPP
 #define SPBSPU_LABS_2025_AADS_CLASSES_B_KIZHIN_EVGENIY_K2_FORWARD_LIST_HPP
 
+#include <cstddef>
+
 namespace kizhin {
   struct FwdList
   {
@@ -9,6 +11,7 @@ namespace kizhin {
   };
 
   FwdList* initList(int, int);
+  FwdList* insertDuplicates(FwdList*, std::size_t, std::size_t);
 }
 
 #endif
