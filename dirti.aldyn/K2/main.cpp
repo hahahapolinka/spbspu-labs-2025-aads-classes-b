cc9ea350e10bf62995627b33d40825df41673950
aldyn-d <aldyndirti@gmail.com>
2025-02-11 00:35:42 +0300

    created starting list

--- dirti.aldyn/K2/main.cpp
+++ dirti.aldyn/K2/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <stdexcept>
 
 struct FwdList
 {
@@ -6,6 +7,40 @@ struct FwdList
   FwdList * next;
 }
 
+void startList(FwdList * head)
+{
+  FwdList * tempHead = head;
+  for (size_t i = 1; i < 10, ++i)
+  {
+    try
+    {
+      tempHead->next = new FwdList{i, nullptr};
+      tempHead = tempHead->next;
+    }
+    catch (const std:bad_alloc & e)
+    {
+      tempHead = head;
+      for (size_t j = 0; j < i; ++j)
+      {
+        FwdList * nextHead = tempHead->next;
+        delete tempHead;
+        tempHead = nextHead;
+      }
+      throw;
+    }
+}
+
 int main()
 {
+  try
+  {
+    FwdList * head = new FwdList{0, nullptr};
+    startList(head);
+  }
+  catch (const std::bad_alloc & e)
+  {
+    std::cerr << "Error" << "\n";
+    return 1;
+  }
+  
 }
