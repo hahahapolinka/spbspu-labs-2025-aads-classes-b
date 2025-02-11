e39b47a0ce31d0f82cf693364553ade96fe0b794
Robotophile <ksyunya.iv@gmail.com>
2025-02-12 02:05:50 +0300

    add clear, print, main

--- ivanova.ksenia/K2/main.cpp
+++ ivanova.ksenia/K2/main.cpp
@@ -8,18 +8,15 @@ struct FwdList {
 FwdList* duplicate(FwdList* head, size_t index, size_t count)
 {
   FwdList* current = head;
-
   for (size_t i = 0; i < index; i++)
   {
     current = current->next;
   }
-
   if (!current)
   {
     std::cerr << "Out of range" << '\n';
     return nullptr;
   }
-
   for (size_t i = 0; i < count; ++i) {
     FwdList* newNode = new FwdList{current->value, current->next};
     current->next = newNode;
@@ -28,7 +25,56 @@ FwdList* duplicate(FwdList* head, size_t index, size_t count)
   return current;
 }
 
+void clear(FwdList* head) 
+{
+  while (head)
+  {
+    FwdList* temp = head;
+    head = head->next;
+    delete temp;
+  }
+}
+
+void print(FwdList* head)
+{
+  std::cout << head->value;
+  while (head->next) {
+    std::cout << " " << head->next->value;
+    head = head->next;
+  }
+  std::cout << "\n";
+}
+
 int main() 
 {
+  FwdList* head = new FwdList{0, nullptr};
+  FwdList* current = head;
+  for (int i = 1; i < 10; ++i) {
+    current->next = new FwdList{i, nullptr};
+    current = current->next;
+  }
 
+  size_t index, count;
+  while (std::cin >> index >> count)
+  {
+    if (index == 0)
+    {
+      std::cerr << "Error" << '\n';
+      clear(head);
+      return 0;
+    }
+    try
+    {
+      duplicate(head, index, count);
+    }
+    catch (const std::exception &e)
+    {
+      std::cerr << "Error" << '\n';
+      clear(head);
+      return 0;
+    }
+  }
+  FwdList* current = head;
+  print(head);
+  return 0;
 }
