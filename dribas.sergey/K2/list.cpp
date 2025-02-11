18a314ed8ce5d2f25bfd3b14501da65563de3de8
Sergey Dribas <serdribas@gmail.com>
2025-02-11 07:45:25 +0300

    add file spliting

---
+++ dribas.sergey/K2/list.cpp
@@ -0,0 +1,58 @@
+#include "list.hpp"
+
+dribas::FwdList* dribas::insert(FwdList* head, int index, size_t count)
+{
+    if (index < 1) {
+      throw std::out_of_range("index is out of range");
+    }
+    FwdList* current = head;
+    for (int i = 1; i < index && current != nullptr; ++i) {
+      current = current->next;
+    }
+    if (!current) {
+      throw std::out_of_range("index is out of range");
+    }
+    for (size_t i = 0; i < count; i++) {
+      FwdList* newNode = new FwdList{current->value, current->next};
+      current->next = newNode;
+      current = newNode;
+    }
+    return head;
+}
+
+dribas::FwdList* dribas::makeList(FwdList* head, int size)
+{
+  try {
+    head = new FwdList {0, nullptr};
+    FwdList* current = head;
+    for (int i = 1; i < size; i++) {
+      current->next = new FwdList {i, nullptr};
+      current = current->next;
+    }
+  } catch (...) {
+    throw;
+  }
+  return head;
+}
+
+void dribas::outList(std::ostream& out, FwdList* head)
+{
+  if (head) {
+    out << head->value;
+    head = head->next;
+    while (head) {
+      out << " " << head->value;
+      head = head->next;
+    }
+  }
+}
+
+void dribas::clear(FwdList* head)
+{
+  FwdList* interim = head;
+  while (head) {
+    interim = head->next;
+    delete head;
+    head = interim;
+  }
+}
--- dribas.sergey/K2/list.hpp
+++ dribas.sergey/K2/list.hpp
@@ -1,5 +1,7 @@
 #ifndef LIST_HPP
 #define LIST_HPP
+#include <cstddef>
+#include <iostream>
 
 namespace dribas
 {
@@ -7,6 +9,11 @@ namespace dribas
     int value;
     FwdList * next;
   };
+
+  FwdList* insert(FwdList* head, int index, size_t count);
+  FwdList* makeList(FwdList* head, int size);
+  void outList(std::ostream& out, FwdList* head);  
+  void clear(FwdList* head);
 }
 
 #endif
