15d7b8c802d4a56616d802f47676e22c393eb05d
Savishna <bravissimo28@mail.ru>
2025-02-24 11:54:03 +0300

    in

--- averenkov.eugene/K4/main.cpp
+++ averenkov.eugene/K4/main.cpp
@@ -3,14 +3,29 @@
 int main(int arg)
 {
   int data;
-  if (!std::cin >> data)
+  std::cin >> data;
+  if (std::cin.fail())
   {
     std::cerr << "error\n";
+    return 1;
+  }
+  if (std::cin.eof())
+  {
     return 0;
   }
-
-  while (std::cin >> input)
+  List* head = new List { data, nullptr };
+  List* current = head;
+  while (std::cin >> data)
   {
-    
+    try
+    {
+      current->next = new List { data, nullptr };
+      current = current->next;
+    }
+    catch (...)
+    {
+      std::cerr << "memory error\n";
+      return 1;
+    }
   }
 }
