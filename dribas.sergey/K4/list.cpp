38869a2a63b249f6e37efe721393c72f69bc1e04
Sergey Dribas <serdribas@gmail.com>
2025-02-24 12:03:44 +0300

    add reverse

--- dribas.sergey/K4/list.cpp
+++ dribas.sergey/K4/list.cpp
@@ -12,11 +12,24 @@ template< class T >
 void dribas::clear(List < T > * head)
 {
   if (head) {
-    FwdList* interim = head;
+    List < T > * interim = head;
     while (head) {
       interim = head->next;
       delete head;
       head = interim;
     }
   }
+}
+
+template< class T >
+dribas::List< T > * dribas::reverse_with_list(List< T > * head)
+{
+  List< T > * stackH = nullptr;
+
+  while(head != nullptr) {
+    List< T > * node = new List < T >{head->data, stackH};
+    stackH = node;
+    head = head.next;
+  }
+  return stackH;
 }
