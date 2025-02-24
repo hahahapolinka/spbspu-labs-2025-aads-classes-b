e666a0783dd6764a00b72003899224d8679d1c78
Robotophile <ksyunya.iv@gmail.com>
2025-02-24 11:20:14 +0300

    intial commit

---
+++ ivanova.ksenia/K4/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main()
+{
+  //They say my hunger's a problem
+  //They tell me to curb my appetite
+  //They say I can't keep myself
+  //From trying a bite of every plate in sight
+  std::cout << "was on the lesson" << '\n';
+}
