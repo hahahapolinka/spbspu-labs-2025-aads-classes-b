41358a0c2da640e9ebc706d4d3032fdf1796e538
Might be a Fox <e.kizhin@yandex.ru>
2025-02-24 15:56:12 +0300

    Complete parseArgs function: add value check

--- kizhin.evgeniy/K4/parse-args.cpp
+++ kizhin.evgeniy/K4/parse-args.cpp
@@ -1,13 +1,22 @@
 #include "parse-args.hpp"
+#include <algorithm>
+#include <array>
+#include <cstring>
 #include <iostream>
 
 int kizhin::parseArgs(std::ostream& out, const int argc, const char* const* const argv)
 {
-  const char* appropriate_values[]{ "0", "1", "2" };
-  if (argc != 2 || argv[1][0] == '\0' || argv[1][1] != '\0') {
-    out << "Usage: " << argv[0] << " <0|1|2>\n";
+  constexpr std::array< const char*, 3 > values{ "0", "1", "2" };
+  bool isValid = argc == 2 && argv[1][0] != '\0' && argv[1][1] == '\0';
+  const auto pred = [&argv](const char* v) -> bool
+  {
+    return std::strcmp(v, argv[1]) == 0;
+  };
+  isValid = isValid && std::any_of(values.begin(), values.end(), pred);
+  if (!isValid) {
+    out << "Usage: " << argv[0] << " [0|1|2]\n";
+    return 0;
   }
-  /* TODO: validate values */
-  return 0;
+  return std::stoi(argv[1]);
 }
 
