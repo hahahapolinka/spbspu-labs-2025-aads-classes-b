c4f29bdba45a83c1789d0ee99ecbcd5088d70e8a
brev1ch <flirktv@gmail.com>
2025-03-10 16:21:54 +0300

    alpha version

--- brevnov.nikita/K5/main.cpp
+++ brevnov.nikita/K5/main.cpp
@@ -7,24 +7,20 @@ struct BiTree {
 };
 
 template< class T >
-class Cmp
+bool comp(const T & a, const T & b)
 {
-public:
-  Cmp(const BiTree< T > & head): value_(head)
-  {} 
-  bool funComp(const T & mem1, const T & mem2)
-  {
-    bool correct = mem1 <= mem2;
-    return correct;
-  }
-private:
-  BiTree< T > value_;
+  return a <= b;
 }
 
-template< class T >
+template< class T, class Cmp >
 void addElTree(BiTree< T > * root, const T & value, Cmp cmp)
 {
   BiTree< T > * sub = root;
+  if (root == nullptr)
+  {
+    root = new BiTree< T >{value, nullptr, nullptr};
+    return;
+  }
   bool done = false;
   while (!done)
   {
@@ -58,40 +54,92 @@ void addElTree(BiTree< T > * root, const T & value, Cmp cmp)
 template< class T >
 void clear(BiTree< T > * head)
 {
-  BiTree< T > * sub = head->left;
-  while (head->left != nullptr && head->right = nullptr)
+  if (head == nulptr)
   {
-    if (sub->left == nullptr && sub->right == nullptr)
-    {
-      delete sub;
-      if (head->left == nullptr)
-      {
-        sub = head->right;
-      }
-      else
-      {
-        sub = head->left;
-      }
-    }
-    else if (sub->left != nullptr)
-    {
-      sub = sub->left;
-    }
-    else
-    {
-      sub = sub->right;
-    }
+    return;
   }
+  clear(head->left);
+  clear(head->right);
   delete head;
 }
 
 template< class T, class Cmp >
 const BiTree< T > * find(const BiTree< T > * root, const T & value, Cmp cmp)
 {
-
+  BiTree< T >  * sub = root;
+  while (sub != nullptr)
+  {
+    if (sub->data == value)
+    {
+      return sub;
+    }
+    else if (cmp(sub->data, value))
+    {
+      sub = sub->right;
+    }
+    else
+    {
+      sub = sub->left;
+    }
+  }
+  return sub;
 }
 
 int main()
 {
-
+  size_t n = 0;
+  std::cin >> n;
+  if (std::cin.fail())
+  {
+    std::cerr << "Not correct input!\n";
+    return 1;
+  }
+  BiList< int > * root = nullptr;
+  int b = 0;
+  for (size_t i = 0; i < n; i++)
+  {
+    std::cin >> b;
+    if (std::cin.fail())
+    {
+      std::cerr << "Not correct input!\n";
+      clear(root);
+      return 1;
+    }
+    if (std::cin.eof())
+    {
+      std::cerr << "Not enough elements!\n";
+      clear(root);
+      return 1;
+    }
+    try
+    {
+      addElTree(root, b, comp)
+    }
+    catch(const std::bad_alloc & e)
+    {
+      std::cerr << "Nor enough memory!\n";
+      clear(root)
+      return 1;
+    }
+    
+  }
+  while (std::cin >> b && std::cin.good())
+  {
+    if (find(root, b, comp))
+    {
+      std::cout << "<FOUND>\n";
+    }
+    else
+    {
+      std::cout << "<NOT FOUND>\n";
+    }
+  }
+  if (std::cin.fail())
+  {
+    std::cerr << "Not correct input!\n";
+    clear(root);
+    return 1;
+  }
+  clear(root);
+  return 0;
 }
