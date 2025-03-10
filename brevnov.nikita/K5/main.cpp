3bc800e9a3c2da1c57ed5614e6de3825378a364e
brev1ch <flirktv@gmail.com>
2025-03-10 11:53:11 +0300

    Bilist

--- brevnov.nikita/K5/main.cpp
+++ brevnov.nikita/K5/main.cpp
@@ -1,3 +1,32 @@
+#include <iostream>
+
+template< class T >
+struct BiTree {
+  T data;
+  BiTree< T > * left, * right;
+};
+
+template< class T >
+class Comperator
+{
+public:
+  Comperator(const BiTree< T > & head): value_(head)
+  {} 
+  bool funComp(const T & member)
+  {
+    bool correct = value_->data < member;
+    return correct;
+  }
+private:
+  BiTree< T > value_;
+}
+
+template< class T, class Cmp >
+const BiTree< T > * find(const BiTree< T > * root, const T & value, Cmp cmp)
+{
+
+}
+
 int main()
 {
 
