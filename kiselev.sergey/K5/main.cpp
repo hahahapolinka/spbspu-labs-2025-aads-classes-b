ca11b24aa4562a016c1a17cd036dfd48d172d94c
deonik3 <kis.sergey2006@gmail.com>
2025-03-10 11:57:23 +0300

    kapec

---
+++ kiselev.sergey/K5/main.cpp
@@ -0,0 +1,117 @@
+#include <iostream>
+#include <new>
+template< class T >
+struct BiTree {
+    T data;
+    BiTree< T >* left, * right;
+};
+
+template< class T >
+bool less(T& num1, T& num2)
+{
+  return num1 < num2;
+}
+
+template< class T, class Cmp >
+BiTree< T >* find(const BiTree< T >* root, const T& value, Cmp cmp)
+{
+  while (root)
+  {
+    if (!cmp(value, root->data))
+    {
+      root = root->right;
+    }
+    else if (cmp(value, root->data))
+    {
+      root = root->left;
+    }
+    else
+    {
+      return root;
+    }
+  }
+  return nullptr;
+}
+
+template< class T, class Cmp >
+void pushTree(BiTree< T >* root, const T& value, Cmp cmp)
+{
+  while(root)
+  {
+    if (cmp(value, root->data))
+    {
+      root = root->left;
+    }
+    else
+    {
+      root = root->left;
+    }
+  }
+  root = new BiTree< T >{ value, nullptr, nullptr };
+}
+
+template< class T >
+void deleteTree(BiTree< T >* root)
+{
+  if (root->left)
+  {
+    deleteTree(root->left);
+  }
+  else if (root->right)
+  {
+    deleteTree(root->right);
+  }
+  delete root;
+}
+
+int main()
+{
+  size_t length = 0;
+  std::cin >> length;
+  if (length == 0)
+  {
+    std::cout << "sequence length 0\n";
+    return 0;
+  }
+  BiTree< int >* root = nullptr;
+  for (size_t i = 0; i < length; ++i)
+  {
+    int number;
+    if (!(std::cin >> number))
+    {
+      std::cerr << "Incorrect number\n";
+      deleteTree(root);
+      return 1;
+    }
+    try
+    {
+      pushTree(root, number, less(number, root->data));
+    }
+    catch (std::bad_alloc&)
+    {
+      deleteTree(root);
+      std::cerr << "Out of memory\n";
+      return 0;
+    }
+  }
+  int findNum;
+  while (std::cin >> findNum)
+  {
+    if (!std::cin.eof() && std::cin.fail())
+    {
+      std::cerr << "Incorrect number\n";
+      deleteTree(root);
+      return 1;
+    }
+    if (find(root, findNum, less(findNum, root->data)))
+    {
+      std::cout << "<FOUND>\n";
+    }
+    else
+    {
+      std::cout << "<NOT FOUND>\n";
+    }
+  }
+  deleteTree(root);
+  return 0;
+}
