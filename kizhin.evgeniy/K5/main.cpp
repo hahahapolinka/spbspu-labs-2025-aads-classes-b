a923f3e80af5aa956edfef508b632076794621f8
Might be a Fox <e.kizhin@yandex.ru>
2025-03-12 12:11:47 +0300

    Fix issue with empty values input

--- kizhin.evgeniy/K5/main.cpp
+++ kizhin.evgeniy/K5/main.cpp
@@ -16,9 +16,12 @@ int main()
       throw std::logic_error("Failed to input sequence");
     }
     int64_t current = 0;
-    while (std::cin >> current) {
-      std::cout << (find(sequence, current) ? "<FOUND>\n" : "<NOT FOUND>\n");
+    if (!(std::cin >> current)) {
+      throw std::logic_error("Failed to input values");
     }
+    do {
+      std::cout << (find(sequence, current) ? "<FOUND>\n" : "<NOT FOUND>\n");
+    } while (std::cin >> current);
     clear(sequence);
   } catch (const std::exception& e) {
     std::cerr << e.what() << '\n';
