11ea2e9c9e63f486cdf5e00feb7e1c11de8adc46
TvoyBatyA12343 <maxdem06@gmail.com>
2025-03-16 16:52:23 +0300

    delete garbagge

--- demehin.maxim/K4/main.cpp
+++ demehin.maxim/K4/main.cpp
@@ -119,9 +119,7 @@ int main(int argc, char* argv[])
 
   if (argc == 2 && argv[1][0] == '0')
   {
-    List< int >* oldHead = head;
     head = reverse_with_list(head);
-    //free_list(oldHead);
   }
   else if (argc == 2 && argv[1][0] == '1')
   {
