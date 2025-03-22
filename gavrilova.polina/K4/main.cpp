65e06c71b840bd11e0d0aee1072415a3705dfdc5
Гаврилова Полина <polina.gavrilova2022@mail.ru>
2025-03-22 23:41:12 +0300

    Fix reverse with list

--- gavrilova.polina/K4/main.cpp
+++ gavrilova.polina/K4/main.cpp
@@ -21,9 +21,10 @@ template< class T >
 FwdList< T >* reverse_with_list(FwdList< T >* head)
 {
   FwdList< FwdList< T >* >* stack = new FwdList< FwdList< T >* >{head, nullptr};
+  head = head->next;
   while (head) {
     try {
-      FwdList< FwdList< int >* >* new_node = new FwdList< FwdList< T >* >{head, stack};
+      FwdList< FwdList< T >* >* new_node = new FwdList< FwdList< T >* >{head, stack};
       stack = new_node;
       head = head->next;
     } catch(const std::bad_alloc&) {
@@ -33,16 +34,17 @@ FwdList< T >* reverse_with_list(FwdList< T >* head)
   }
   head = stack->data;
   FwdList< T >* current_head = head;
-  FwdList< FwdList< T >* >* cur_stack = stack;
-  while (cur_stack->next) {
-    current_head->next = cur_stack->next->data;
-    cur_stack = cur_stack->next;
+  FwdList< FwdList< T >* >* cur_stack = stack->next;
+  while (cur_stack) {
+    current_head->next = cur_stack->data;
     current_head = current_head->next;
-    delete stack;
-    stack = cur_stack;
+    FwdList< FwdList< T >* >* temp_stack = cur_stack;
+    cur_stack = cur_stack->next;
+    delete temp_stack;
   }
+  delete stack;
+  current_head->next = nullptr;
   return head;
-
 }
 
 template< class T >
@@ -50,19 +52,19 @@ FwdList< T >* reverse_cleanly(FwdList< T >* head) noexcept {
   if (!head) {
     return nullptr;
   }
-  FwdList< T >* cur = head->next;
-  if (!cur) {
-    return head;
+
+  FwdList< T >* prev = nullptr;
+  FwdList< T >* cur = head;
+  FwdList< T >* next = nullptr;
+
+  while (cur) {
+    next = cur->next;
+    cur->next = prev;
+    prev = cur;
+    cur = next;
   }
-  FwdList< T >* temp = cur->next;
-  head->next = nullptr;
-  while (temp) {
-    cur->next = head;
-    head = cur;
-    cur = temp;
-    temp = temp->next;
-  }
-  return cur;
+
+  return prev; 
 }
 
 template<class T>
@@ -82,12 +84,12 @@ FwdList<T>* reverse_recursively(FwdList<T>* head) noexcept {
 FwdList< int >* inputListInt(std::istream& in)
 {
   int newData = 0;
-  if (!(std::cin >> newData)) {
+  if (!(in >> newData)) {
     return nullptr;
   }
   FwdList< int >* head = new FwdList< int >{newData, nullptr};
   FwdList< int >* cur = head;
-  while (in >> newData && !in.eof()) {
+  while (in >> newData || !in.eof()) {
     try {
       FwdList< int >* new_node = new FwdList< int >{newData, nullptr};
       cur->next = new_node;
@@ -123,7 +125,7 @@ int main (int argc, char** argv)
   }
   FwdList< int >* new_head = nullptr;
 
-  if (argc == 2 && argv[1][0] == '0')
+  if (argc == 2 && std::string(argv[1]) == "0")
   {
     try {
       new_head = reverse_with_list(head);
@@ -132,11 +134,11 @@ int main (int argc, char** argv)
       return 1;
     }
   }
-  else if (argc == 2 && argv[1][0] == '1')
+  else if (argc == 2 && std::string(argv[1]) == "1")
   {
     new_head = reverse_cleanly(head);
   }
-  else if (argc == 2 && argv[1][0] == '2')
+  else if (argc == 2 && std::string(argv[1]) == "2")
   {
     new_head = reverse_recursively(head);
   }
@@ -146,5 +148,6 @@ int main (int argc, char** argv)
     new_head = reverse_cleanly(head);
   }
   outputListInt(std::cout, new_head);
+  std::cout << " ";
   clear< int >(new_head);
 }
