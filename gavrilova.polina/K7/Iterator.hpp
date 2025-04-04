3d328b8c1e969249a8a7d52ba7a2a2e9009916a1
Гаврилова Полина <polina.gavrilova2022@mail.ru>
2025-04-05 00:21:03 +0300

    K7

---
+++ gavrilova.polina/K7/Iterator.hpp
@@ -0,0 +1,122 @@
+#ifndef ITERATOR_HPP
+#define ITERATOR_HPP
+
+#include "BiTree.hpp"
+namespace gavrilova {
+
+  template< class T >
+  struct BiTreeIterator {
+    BiTree< T >* node;
+
+    bool hasPrev() const;
+    bool hasNext() const;
+    BiTreeIterator< T > next() const;
+    BiTreeIterator< T > prev() const;
+
+    const T & data() const;
+  };
+  
+  template< class T, class Cmp = std::less< T > >
+  BiTreeIterator< T > begin(BiTree< T, Cmp >* root);
+
+  template< class T, class Cmp = std::less< T > >
+  BiTreeIterator< T > rbegin(BiTree< T, Cmp >* root);
+
+  template< class T >
+  bool BiTreeIterator< T >::hasPrev() const
+  {
+    return prev().node != nullptr;
+  }
+
+  template< class T >
+  bool BiTreeIterator< T >::hasNext() const
+  {
+    return next().node != nullptr;
+  }
+
+
+  template< class T >
+  BiTreeIterator< T > BiTreeIterator< T >::next() const
+  {
+    if (!node) {
+      return BiTreeIterator< T >{node};
+    }
+
+    if (node->right) {
+      BiTree< T >* cur = node->right;
+      while (cur->left) {
+        cur = cur->left;
+      }
+      return BiTreeIterator< T >{cur};
+    } else {
+      BiTree< T >* prev = node;
+      BiTree< T >* cur = prev->parent;
+      while (cur && cur->cmp(prev->data, cur->data)) {
+        prev = cur;
+        cur = cur->parent;
+      }
+      if (cur) {
+        return BiTreeIterator< T >{cur};
+      } else {
+        return BiTreeIterator< T >{nullptr};
+      }
+    }
+  }
+
+  template< class T >
+  BiTreeIterator< T > BiTreeIterator< T >::prev() const
+  {
+    if (!node) {
+      return BiTreeIterator< T >{node};
+    }
+
+    if (node->left) {
+      BiTree< T >* cur = node->left;
+      while (cur->right) {
+        cur = cur->right;
+      }
+      return BiTreeIterator< T >{cur};
+    } else {
+      BiTree< T >* prev = node;
+      BiTree< T >* cur = prev->parent;
+      while (cur && cur->cmp(cur->data, prev->data)) {
+        prev = cur;
+        cur = cur->parent;
+      }
+      if (cur) {
+        return BiTreeIterator< T >{cur};
+      } else {
+        return BiTreeIterator< T >{nullptr};
+      }
+    }
+  }
+
+  template< class T >
+  const T & BiTreeIterator< T >::data() const
+  {
+    return node->data;
+  }
+
+  template< class T, class Cmp = std::less< T > >
+  BiTreeIterator< T > begin(BiTree< T >* root)
+  {
+    if (root) {
+      while (root->left) {
+        root = root->left;
+      }
+    }
+    return BiTreeIterator< T >{root};
+  }
+
+  template< class T, class Cmp = std::less< T > >
+  BiTreeIterator< T > rbegin(BiTree< T >* root)
+  {
+    if (root) {
+      while (root->right) {
+        root = root->right;
+      }
+    }
+    return BiTreeIterator< T >{root};
+  }
+}
+#endif
--- gavrilova.polina/K7/main.cpp
+++ gavrilova.polina/K7/main.cpp
@@ -1,3 +1,8 @@
+#include <iostream>
+#include <string>
+#include "BiTree.hpp"
+#include "Iterator.hpp"
+
 int main()
 {
   size_t n = 0;
@@ -5,6 +10,46 @@ int main()
     std::cerr << "Invalid input!\n";
     return 1;
   }
+  gavrilova::BiTree< size_t >* root = nullptr;
+  for (; n > 0; --n) {
+    size_t cur_num = 0;
+    if (!(std::cin >> cur_num)) {
+      std::cerr << "Invalid input!\n";
+      gavrilova::clear< size_t >(root);
+      return 1;
+    }
+    try {
+      root = gavrilova::insertBT< size_t >(cur_num, root);
+    } catch(const std::bad_alloc&) {
+      gavrilova::clear< size_t >(root);
+      std::cerr << "Memory error!\n";
+      return 1;
+    }
+  }
 
+  std::string way_to_output = "";
+  std::cin >> way_to_output;
   
+  
+  if (way_to_output == "tomin") {
+    auto it = gavrilova::begin< size_t >(root);
+    while (it.hasNext()) {
+      std::cout << it.data() << " ";
+      it = it.next();
+    }
+    std::cout << it.data();
+  } else if (way_to_output == "tomax") {
+    auto it = gavrilova::rbegin< size_t >(root);
+    while (it.hasPrev()) {
+      std::cout << it.data() << " ";
+      it = it.prev();
+    }
+    std::cout << it.data();
+  } else {
+    gavrilova::clear< size_t >(root);
+    std::cerr << "Invalid command\n";
+    return 1;
+  }
+  std::cout << "\n";
+  gavrilova::clear< size_t >(root);
 }
